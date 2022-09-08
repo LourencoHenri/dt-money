6293ed20b18b0b32463dc104bf40b69536c0f3fa
Lourenço <henri.lourenco@outlook.com>
2022-09-08 18:43:45 -0300

    first

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from "../../assets/logo.svg"
+import { Container } from "./styles"
+import { Content } from "./styles"
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal} : HeaderProps) {
+    
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type="button" onClick={onOpenNewTransactionModal}>
+                    Nova transação
+                </button>
+            </Content>            
+        </Container>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from './App';
+import { createServer, Model } from "miragejs"
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Moradia",
+          amount: 750,
+          createdAt: new Date("2022-07-07 23:00:00"),
+        },
+        {
+          id: 2,
+          title: "Criptoativos",
+          type: "deposit",
+          category: "Renda",
+          amount: 2040.54,
+          createdAt: new Date("2022-10-07 23:17:05"),
+        },    
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = "api"
+
+    this.get("/transactions", () => {
+
+      
+      return this.schema.all("transaction");
+    })
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create("transaction", data)
+    })
+
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +58,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
